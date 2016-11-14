dfa4f7b11a53f2dba6c676c2b16425b222f374d9
Tanmay Awasthi <Tanmay Awasthi>
2016-11-14 22:23:10 +0530

    Generic json to fetch zodiac name and content

--- js/horoscope.js
+++ js/horoscope.js
@@ -1,25 +1,42 @@
 /**
  * Created by Tan$ on 11/13/2016.
  */
-app.controller('HoroscopeCtrl', function($scope,$mdDialog){
+app.controller('HoroscopeCtrl', function ($scope, $mdDialog, $http) {
     console.log("Horoscope controller reporting on duty");
 
-    $scope.popover=function(code){
-        console.log(code);
+    $scope.zodiacPopover = function (code) {
+        var zodiacContent = null;
+        var zodiacName = null;
+        $http.get('resources/horoscopeContent.json').success(function (data) {
 
-            $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'templates/AriesTemplate.html',
-                parent: angular.element(document.body),
-                clickOutsideToClose:true,
-                hasBackdrop:true,
-                escapeToClose:true,
-                focusOnOpen:false,
-                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+            var zodiacData=[];
+            angular.forEach(data, function(item){
+                zodiacData.push(item);
             })
 
-        }
-    function DialogController($scope, $mdDialog) {
+            $mdDialog.show({
+                locals: {code: code, zodiacName: zodiacData[code-1].name, zodiacContent: zodiacData[code-1].content},
+                controller: DialogController,
+                templateUrl: 'templates/ZodiacTemplate.tmpl.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose: true,
+                hasBackdrop: true,
+                escapeToClose: true,
+                focusOnOpen: false,
+                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+            })
+        });
+
+
+
+    }
+    function DialogController($scope, $mdDialog, code, zodiacName, zodiacContent) {
+        console.log("Code  " + code);
+        console.log("Name " + zodiacName);
+        console.log("Content " + zodiacContent);
+        $scope.code = code;
+        $scope.content=zodiacContent;
+        $scope.name=zodiacName;
         $scope.hide = function () {
             $mdDialog.hide();
         };
